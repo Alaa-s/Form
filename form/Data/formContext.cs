a4cb544da7d84ad2f8c0a25655d5bd2b63bd0180
Alaa-s <alaaosama519@gmail.com>
2021-02-02 22:19:38 +0200

    first commit

---
+++ form/Data/formContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using form.Models;
+
+namespace form.Data
+{
+    public class formContext : DbContext
+    {
+        public formContext (DbContextOptions<formContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<form.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ form/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace form.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string gender { get; set; }
+        
+        public string Country { get; set; }
+        public string City { get; set; }
+    }
+}
